728560c5eb9e075dc4ce0910ba35e50cbce4df2c
ThuF <jordan_p_pavlov@abv.bg>
2016-01-22 16:12:47 +0200

    Adds JavaDoc for the IJPAEntity interface

--- src/main/java/tu/sofia/cv/entity/IJPAEntity.java
+++ src/main/java/tu/sofia/cv/entity/IJPAEntity.java
@@ -1,7 +1,17 @@
 package tu.sofia.cv.entity;
 
+/**
+ * Interface used for marking the JPA Entities
+ * 
+ * @param <Key>
+ */
 public interface IJPAEntity<Key> {
 
+	/**
+	 * Returns the value of the JPA key
+	 * 
+	 * @return the value of the JPA key
+	 */
 	public Key getKeyValue();
 
 }
