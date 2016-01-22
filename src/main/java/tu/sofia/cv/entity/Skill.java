450f828fcd224fb9270b387ab4aa3568cafc7060
ThuF <jordan_p_pavlov@abv.bg>
2016-01-22 12:49:24 +0200

    Adds Skill entity

---
+++ src/main/java/tu/sofia/cv/entity/Skill.java
@@ -0,0 +1,65 @@
+package tu.sofia.cv.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Entity class representing the T_SKILL database table
+ */
+@Entity
+@Table(name = "T_SKILL")
+public class Skill implements Serializable {
+
+	private static final long serialVersionUID = 8733337025341018485L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long skillId;
+
+	@Column(length = 150, nullable = false)
+	private String name;
+
+	/**
+	 * Returns the skill Id
+	 *
+	 * @return the skill Id
+	 */
+	public Long getSkillId() {
+		return skillId;
+	}
+
+	/**
+	 * Sets the skill Id
+	 *
+	 * @param skillId
+	 *            the skill Id
+	 */
+	public void setSkillId(Long skillId) {
+		this.skillId = skillId;
+	}
+
+	/**
+	 * Returns the name
+	 *
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Sets the name
+	 *
+	 * @param name
+	 *            the name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}
