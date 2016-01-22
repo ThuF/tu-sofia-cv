e04b6e3cfa2977d91bd9ba672dd491dd30620253
ThuF <jordan_p_pavlov@abv.bg>
2016-01-22 17:08:20 +0200

    Fixes EducationDao

--- src/main/java/tu/sofia/cv/dao/EducationDao.java
+++ src/main/java/tu/sofia/cv/dao/EducationDao.java
@@ -3,17 +3,17 @@ package tu.sofia.cv.dao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import tu.sofia.cv.entity.Skill;
+import tu.sofia.cv.entity.Education;
 
 /**
- * DAO class for {@link Skill}
+ * DAO class for {@link Education}
  */
-public class EducationDao extends AbstractJpaDao<Long, Skill> {
+public class EducationDao extends AbstractJpaDao<Long, Education> {
 
 	private static final Logger logger = LoggerFactory.getLogger(EducationDao.class);
 
 	protected EducationDao() {
-		super(Skill.class);
+		super(Education.class);
 	}
 
 	@Override
